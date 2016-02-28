cd70cef2aa1bf8704994e16852db7843783569a3
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2016-02-28 19:32:49 +0000

    Added control with arrow keys and change of direction

--- snake.js
+++ snake.js
@@ -6,17 +6,42 @@ var drawSnake = function(snakeToDraw) {
 	CHUNK.draw(drawableObjects);
 //call draw on drawableObjects. 
 }
+
+var moveSegment = function(segment) {
+  if (segment.direction === "down") {
+    return { top: segment.top + 1, left: segment.left }
+  } else if (segment.direction === "up") {
+    return { top: segment.top - 1, left: segment.left }
+  } else if (segment.direction === "right") {
+    return { top: segment.top, left: segment.left + 1 }
+  } else if (segment.direction === "left") {
+    return { top: segment.top, left: segment.left - 1 }
+  }
+  return segment;
+}
 var moveSnake = function(snake) {
   var oldSegment = snake[0];//get the value at this location in the array
-  var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };
+  var newSegment = moveSegment(oldSegment);
+  newSegment.direction = oldSegment.direction;
   var newSnake = [newSegment];
   return newSnake;
 }
 var advanceGame = function() {
   snake = moveSnake(snake);
+  if (CHUNK.detectCollisionBetween(snake, CHUNK.gameBoundaries())) {
+    CHUNK.endGame();
+    CHUNK.flashMessage("Whoops! You Hit a Wall!");
+  }
   drawSnake(snake);
 }
-var snake = [{ top: 0, left: 0 }];
+var changeDirection = function(direction) {
+  snake[0].direction = direction;
+}
+
+var snake = [{ top: 0, left: 0, direction: "down" }];
 CHUNK.executeNTimesPerSecond(advanceGame, 1);
+CHUNK.onArrowKey(changeDirection);
+
+
 
 
