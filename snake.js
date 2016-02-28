1fba9bb3d7a040ec93106c21f10646a7ed6e3039
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2016-02-28 22:48:05 +0000

    Added apple creation

--- snake.js
+++ snake.js
@@ -1,8 +1,8 @@
-var drawSnake = function(snakeToDraw) {
+var draw = function(snakeToDraw, apple) {
 	var drawableSnake = { color: "green", pixels: snakeToDraw };
 	//creates and object specifying color and pixels of drawableSnake
-	var drawableObjects = [drawableSnake];
-	//CHUNK's draw method expects an array (forEach method). we put the drawableSnake in an array here
+  var drawableApple = { color: "red", pixels: [apple] }; //because CHUNK's draw fxn expects array
+	var drawableObjects = [drawableSnake, drawableApple]; //because CHUNK's draw fxn expects array
 	CHUNK.draw(drawableObjects);
 //call draw on drawableObjects. 
 }
@@ -39,13 +39,14 @@ var advanceGame = function() {
     CHUNK.endGame();
     CHUNK.flashMessage("Whoops! You Hit a Wall!");
   }
-  drawSnake(snake);
+  draw(snake, apple);
 }
 var changeDirection = function(direction) {
   snake[0].direction = direction;
 }
 
 var snake = [{ top: 1, left: 0, direction: "down" }, { top: 0, left: 0, direction: "down" }];
+var apple = { top: 8, left: 10 };
 CHUNK.executeNTimesPerSecond(advanceGame, 1);
 CHUNK.onArrowKey(changeDirection);
 
