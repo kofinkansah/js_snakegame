2eb82374df1e0369eca9abdcc23434bd39a2bf41
Kofi Appiah-Nkansah <kofinkansah@gmail.com>
2016-02-28 23:02:55 +0000

    Snake can now eat apple and grow

--- snake.js
+++ snake.js
@@ -35,6 +35,11 @@ var moveSnake = function(snake) {
 }
 var advanceGame = function() {
   snake = moveSnake(snake);
+  if (CHUNK.detectCollisionBetween([apple], snake)) {
+    snake = growSnake(snake);
+    apple = CHUNK.randomLocation();
+  };
+
   if (CHUNK.detectCollisionBetween(snake, CHUNK.gameBoundaries())) {
     CHUNK.endGame();
     CHUNK.flashMessage("Whoops! You Hit a Wall!");
@@ -43,6 +48,12 @@ var advanceGame = function() {
 }
 var changeDirection = function(direction) {
   snake[0].direction = direction;
+}
+var growSnake = function(snake) {
+  var indexOfLastSegment = snake.length - 1;
+  var lastSegment = snake[indexOfLastSegment];
+  snake.push({ top: lastSegment.top, left: lastSegment.left });
+  return snake;
 }
 
 var snake = [{ top: 1, left: 0, direction: "down" }, { top: 0, left: 0, direction: "down" }];
